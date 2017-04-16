05399762913584b8d6f3ecf8326d06f89ef1b352
Asheesh Chaudhary <ac4149@gmail.com>
2017-04-17 00:24:19 +0530

    Add files via upload

---
+++ client/js/frontApp.js
@@ -0,0 +1,9 @@
+var betApp = angular.module('betApp', []);
+
+betApp.controller('HeaderController', ['$scope', function($scope){
+
+}]);
+
+betApp.controller('FooterController', ['$scope', function($scope){
+
+}]);
